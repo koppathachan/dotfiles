ab417b15619783ccc1b561a4052a661cf653b1fa
sasidakh <akhilsasidharan1991@gmail.com>
2021-04-24 13:31:25 +0530

    back to garuda

--- emacs/.doom.d/custom.el
+++ emacs/.doom.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("aaa4c36ce00e572784d424554dcc9641c82d1155370770e231e10c649b59a074" default)))
+   '("c086fe46209696a2d01752c0216ed72fd6faeabaaaa40db9fc1518abebaf700d" "b5fff23b86b3fd2dd2cc86aa3b27ee91513adaefeaa75adc8af35a45ffb6c499" "ca70827910547eb99368db50ac94556bbd194b7e8311cfbdbdcad8da65e803be" "aaa4c36ce00e572784d424554dcc9641c82d1155370770e231e10c649b59a074" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
