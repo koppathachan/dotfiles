4e33a75cccca941ae6cb955ffa8e24602ce3b7ec
koppathachan <akhilsasidharan1991@gmail.com>
2022-10-09 10:55:14 +0530

    emacs from scratch

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,48 +1,236 @@
-;;; init.el --- emacs configuration continued
-;;; Commentary:
-;; config that changes a lot
+;; The default is 800 kilobytes.  Measured in bytes.
+(setq gc-cons-threshold (* 50 1000 1000))
 
-;;; Code:
+;; Profile emacs startup
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "*** Emacs loaded in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
+
+;; Silence compiler warnings as they can be pretty disruptive
+(setq comp-async-report-warnings-errors nil)
+
+;; You will most likely need to adjust this font size for your system!
+(defvar runemacs/default-font-size 120)
 
 (setq inhibit-startup-message t)
-(setq backup-directory-alist '(("" . "~/.emacs.d/backup")))
-(setq auto-save-default t)
-(setq auto-save-visited-mode t)
-(setq create-lockfiles nil)
 
-;; Disable tool bar, menu bar, scroll bar.
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 0)        ; Give some breathing room
 
+(menu-bar-mode -1)            ; Disable the menu bar
+
+;; Set up the visible bell
+(setq visible-bell t)
+
+(set-face-attribute 'default nil :font "Fira Code Retina" :height runemacs/default-font-size)
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Disable line numbers for some modes
+(dolist (mode '(term-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;;{{{ Set up package and use-package
+
+;; Initialize package sources
 (require 'package)
-
-;;; Code:
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
- 
-(setq package-enable-at-startup nil)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("melpa-stable" . "https://stable.melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 
+;; Bootstrap 'use-package'
+(eval-after-load 'gnutls
+  '(add-to-list 'gnutls-trustfiles "/etc/ssl/cert.pem"))
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
+(unless package-archive-contents
+  (package-refresh-contents))
 (eval-when-compile
   (require 'use-package))
+(require 'bind-key)
+(setq use-package-always-ensure t)
+
+;;}}}
+
+;; Bootstrap straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+      (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+        "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+        'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Always use straight to install on systems other than Linux
+(setq straight-use-package-by-default (not (eq system-type 'gnu/linux)))
+
+;; Use straight.el for use-package expressions
+(straight-use-package 'use-package)
+
+;; Load the helper package for commands like `straight-x-clean-unused-repos'
+(require 'straight-x)
+
+(set-default-coding-systems 'utf-8)
+
+(server-start)
+
+(use-package command-log-mode)
+
+(use-package diminish)
+
+(use-package hydra
+  :defer 1)
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+;; search
+(use-package swiper
+  :ensure t)
+
+(use-package doom-themes
+  :init (load-theme 'doom-dracula t))
+
+;; NOTE: The first time you load your configuration on a new machine, you'll
+;; need to run the following command interactively so that mode line icons
+;; display correctly:
+;;
+;; M-x all-the-icons-install-fonts
+(use-package all-the-icons)
+
+;; simple modeline from doom
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+
+;; colours for all programming languages
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(defun dw/replace-unicode-font-mapping (block-name old-font new-font)
+  (let* ((block-idx (cl-position-if
+                         (lambda (i) (string-equal (car i) block-name))
+                         unicode-fonts-block-font-mapping))
+         (block-fonts (cadr (nth block-idx unicode-fonts-block-font-mapping)))
+         (updated-block (cl-substitute new-font old-font block-fonts :test 'string-equal)))
+    (setf (cdr (nth block-idx unicode-fonts-block-font-mapping))
+          `(,updated-block))))
+
+(use-package unicode-fonts
+  :disabled
+  :if (not dw/is-termux)
+  :custom
+  (unicode-fonts-skip-font-groups '(low-quality-glyphs))
+  :config
+  ;; Fix the font mappings to use the right emoji font
+  (mapcar
+    (lambda (block-name)
+      (dw/replace-unicode-font-mapping block-name "Apple Color Emoji" "Noto Color Emoji"))
+    '("Dingbats"
+      "Emoticons"
+      "Miscellaneous Symbols and Pictographs"
+      "Transport and Map Symbols"))
+  (unicode-fonts-setup))
+
+;; shows me the commands from any key binding.
+;; for eg press C-x and the panel will pop up with the key bindings starting with C-x
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+;; this is required for ivy-rich
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-switch-buffer)
+         ("C-M-j" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history)))
+
+(use-package ivy-hydra
+  :defer t
+  :after hydra)
+
+;; add some info to the ivy M-x and C-x C-f
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+;; replace emacs help with helpful help
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+(use-package general)
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  ;; NOTE: Set this to the folder where you keep your Git repos!
+  (when (file-directory-p "~/code")
+    (setq projectile-project-search-path '("~/code")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :init
+  (setq lsp-keymap-prefix "C-c l")  ;; Or 'C-l', 's-l'
+  :config
+  (lsp-enable-which-key-integration t))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(custom-enabled-themes '(misterioso))
- '(inhibit-startup-screen t)
- '(js-indent-level 2)
  '(package-selected-packages
-   '(ivy js2-mode flycheck typescript-mode hindent haskell-mode exec-path-from-shell xref-js2 use-package tide tern swiper ripgrep projectile magit js2-refactor go-mode company)))
+   '(which-key use-package rainbow-delimiters magit ivy-rich helpful general doom-themes doom-modeline counsel-projectile command-log-mode all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -50,104 +238,27 @@
  ;; If there is more than one, they won't work right.
  )
 
-(global-display-line-numbers-mode)
-
-(use-package exec-path-from-shell
-  :ensure t)
-(exec-path-from-shell-initialize)
-
-(use-package haskell-mode
-  :ensure t)
-(use-package hindent
-  :ensure t)
-
-;;(server-start)
+;; run npm install -g typescript-language-server
 (use-package typescript-mode
-  :ensure t)
-(use-package go-mode
-  :init
-  (add-hook 'go-mode-hook
-      (lambda ()
-        (setq tab-width 4)))
+  :mode "\\.ts\\'"
+  :hook (typescript-mode . lsp-deferred)
   :config
-  (add-hook 'before-save-hook #'gofmt-before-save)
-  :ensure t)
+  (setq typescript-indent-level 2))
 
-(use-package ripgrep
-  :ensure t)
-;;TODO use lsp-mode for this
-;; This package requires eslint to be installed globally.
-(use-package flycheck
-  :ensure t
+(use-package alert
+  :commands alert
   :config
-  (flycheck-add-mode 'javascript-eslint 'js-mode)
-  :init
-  (global-flycheck-mode))
+  (setq alert-default-style 'notifications))
 
-(add-to-list 'display-buffer-alist
-             `(,(rx bos "*Flycheck errors*" eos)
-              (display-buffer-reuse-window
-               display-buffer-in-side-window)
-              (side            . bottom)
-              (reusable-frames . visible)
-              (window-height   . 0.33)))
-
-(use-package company
-  :ensure t
-  :init
-  (setq company-idle-delay nil  ; avoid auto completion popup, use TAB
-                                ; to show it
-        company-tooltip-align-annotations t)
-  :hook (after-init . global-company-mode)
-  :bind (:map company-active-map
-	 ("<return>" . "")
-         ("C-n" . company-select-next)
-         ("C-p" . company-select-previous))
+(use-package super-save
+  :defer 1
+  :diminish super-save-mode
   :config
-  (setq company-idle-delay 0))
+  (super-save-mode +1)
+  (setq super-save-auto-save-when-idle t))
 
-(use-package tide
-  :ensure t
-  :after typescript-mode
-  :hook ((typescript-mode . tide-setup)
-         (typescript-mode . tide-hl-identifier-mode)
-         (before-save . tide-format-before-save)))
+(setq-default tab-width 2)
+(setq-default evil-shift-width tab-width)
 
-;;(use-package evil)
-(use-package js2-mode
-  :ensure t
-  :mode "\\.js\\'"
-  :config
-  (define-key js-mode-map (kbd "M-.") nil)
-  (setq-default js2-ignored-warnings '("msg.extra.trailing.comma")))
-
-(use-package js2-refactor
-  :ensure t
-  :config
-  (js2r-add-keybindings-with-prefix "C-c C-r")
-  (add-hook 'js2-mode-hook 'js2-refactor-mode))
-  ;;(add-hook 'js2-mode-hook (lambda ()
-  ;;(add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
-
-(use-package xref-js2
-  :ensure t)
-(use-package magit
-  :bind ("C-x g" . magit-status)
-  :ensure t)
-(use-package ivy
-  :ensure t)
-(use-package swiper
-  :ensure t
-  :config
-  (global-set-key "\C-s" 'swiper)
-  (global-set-key "\C-r" 'swiper-backward))
-;; use eslint with web-mode for jsx files
-
-(use-package projectile
-  :ensure t
-  :config
-  (setq projectile-completion-system 'ivy)
-  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-  (global-set-key "\C-x\C-f" 'projectile-find-file)
-  (projectile-mode +1))
-;;; init.el ends here
+(use-package origami
+  :hook (yaml-mode . origami-mode))
